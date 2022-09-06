1cba59b02626080fa69f26c3707c8aa35fa3dce4
Michele M <mnmin@gmail.com>
2022-09-06 12:12:53 +0100

    Completed

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -4,16 +4,27 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const prisma = require('../utils/prisma.js')
+const saltRounds = 10
 
 router.post('/', async (req, res) => {
     // Get the username and password from request body
+    const { username, password } = req.body
+
+    const encryptedPw = await bcrypt.hash(password, saltRounds)
+
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            password: encryptedPw
+        }
+    })
     
     // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
     
     // Save the user using the prisma user model, setting their password to the hashed version
     
     // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    res.status(201).json({ user: {username: newUser.username, id: newUser.id} })
 });
 
 module.exports = router;
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -4,11 +4,31 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const prisma = require('../utils/prisma.js')
+const secretKey = process.env.JWT_SECRET
 
 router.post('/', async (req, res) => {
     const { username, password } = req.body;
     // Get the username and password from the request body
 
+    const userExists = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    if(!userExists) {
+        res.status(401).json({error: "error user does not exist"})
+    }
+
+    const hashValidId = await bcrypt.compare(password, userExists.password)
+
+    if(!hashValidId) {
+        res.status(401).json({error: "error invalid password"})
+    }
+
+    const token = jwt.sign({username}, secretKey)
+
+    res.status(200).json({token})
     // Check that a user with that username exists in the database
     // Use bcrypt to check that the provided password matches the hashed password on the user
     // If either of these checks fail, respond with a 401 "Invalid username or password" error
